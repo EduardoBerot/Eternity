bfadabb8cbc46757515434d128ef255a73a4e73d
tiagolimar <tiagolima.r2@gmail.com>
2024-04-21 11:35:14 -0300

    update url

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,8 @@
 const URL_DISCORD_INTEGRATION = "https://discord.com/api/webhooks/1228837417688109179/8LEi6HLNmY-nHpn0rB9oKk2fBrC0QzFQ334CToKoFnVOZxDmk8Vko0odRdjC34P6bSzE"
 // const URL_DISCORD_INTEGRATION = "https://discord.com/api/webhooks/1228845807550074900/HwKlDWuMZqONWv1HumZnjIoBPJnYmuDKnWziKJbImSe2EuWf6PsvswlCHPLYph24FWvH"
 
-const URL_BASE = "http://localhost:5000/api/membro"
+// const URL_BASE = "http://localhost:5000/api/membro"
+const URL_BASE = "https://eternity-crud.onrender.com/api/membros"
 
 async function send (event){
     event.preventDefault();
