c576afb7441cd494216fe2b8fc9712132416956b
tiagolimar <tiagolima.r2@gmail.com>
2024-05-03 12:34:52 -0300

    fix: resolve issue #19

--- js/auth.js
+++ js/auth.js
@@ -3,22 +3,10 @@ async function send (event){
     
     const ETY_FORM_COOKIE = 'eternity-form';
     const ETY_FORM_TIME = 10; /* tempo em minutos */
-    const URL_MEMBERS = `${URL_BASE}/api/membro`
+    const URL_MEMBERS = `${URL_BASE}/api/membro`;
 
     if (!checkCookie(ETY_FORM_COOKIE)){
         data = getFormData();
-        
-        const body = {
-            content: "Mensagem Recebida",
-            tts: false,
-            color: "white",
-            embeds: [
-                {
-                    title: "Formulário de Cadastro",
-                    description: JSON.stringify(data,null,'\t'),
-                },
-            ],
-        };
 
         try {
             let response = await fetch(URL_MEMBERS,
@@ -33,22 +21,8 @@ async function send (event){
     
             if (!response.ok) {
                 throw new Error("Network response was not ok");
-            }else{
-                response = await fetch(URL_DISCORD_INTEGRATION,
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify(body),
-                    }
-                );
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
             }
-            
-            // const data = await response.json();
+
             setCookie(ETY_FORM_COOKIE, ETY_FORM_TIME);
             alert("Cadastro Realizado com sucesso!");
             msg.innerText="Cadastro Realizado com sucesso!";
