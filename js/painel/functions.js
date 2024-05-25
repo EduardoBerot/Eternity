97c04b19c1e3343afe57e8e0b8113e950efdd40d
tiagolimar <tiagolima.r2@gmail.com>
2024-05-25 06:48:29 -0300

    fix: resolve issue #29

--- js/painel/functions.js
+++ js/painel/functions.js
@@ -29,7 +29,11 @@ function hideLoading() {
 }
 
 function renderLoading(element) {
-    element.innerHTML += '<div id="loading">Carregando<span class="loading-dot">...</span></div>'
+    element.innerHTML += `
+    <div id="loading" >Carregando
+        <span class="loading-dot">...</span>
+        <div class="alert" style="font-size:0.75em;margin-left:auto;margin-top:0.5em;">(isso pode demorar um pouco)</div>
+    </div>`
 }
 
 function createTable(element,id) {
