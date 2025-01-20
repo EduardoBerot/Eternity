b348669de11fb110d43bea80b2a2bb9763abe6bf
tiagolimar <tiagolima.r2@gmail.com>
2025-01-20 20:02:52 -0300

    fix: organizacao de scripts

--- js/aniver.js
+++
@@ -1,9 +0,0 @@
-
-function checkAniver() {
-    const URL_ANIVERSARIANTES = `${URL_BASE}/api/aniversariantes`
-    fetch(URL_ANIVERSARIANTES)
-        .then(response=>{
-            return response.json()
-        })
-        .catch(error=>console.error(`Um erro ocorreu: ${error}`))
-}
