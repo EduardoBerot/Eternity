dea83e5acecf0c4fb6c858e891cb6ae2189507ba
Eduardo <eduardocaneppele74@gmail.com>
2026-08-09 23:32:08 -0300

    fix: prevent negative membership days

--- js/home/index.js
+++ js/home/index.js
@@ -188,7 +188,7 @@ function calculateInYearOrDays(incomeDate) {
         const today = new Date();
         const timeDifference = today - new Date(birthDate);
         const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        return daysDifference;
+        return Math.max(0, daysDifference);
     }
 
     incomeDate = new Date(incomeDate);
